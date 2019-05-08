2eab36337131b08b210711b596d22c2ef3038ea1
ehoseini <ehoseini@mit.edu>
2019-05-07 20:50:01 -0400

    fix

--- om_preprocess.sh
+++ om_preprocess.sh
@@ -23,3 +23,8 @@ dicom_suffix=_dicoms
 echo $SLURM_ARRAY_TASK_ID
 subject_id=$(awk -v var="$SLURM_ARRAY_TASK_ID" 'BEGIN{ RS = "" ; FS = "\n" }{print $var}' subjects.txt)
 echo $subject_id
+
+mkdir "$data_dir$subject_id$suffix"
+analysis_dir="$data_dir$subject_id$suffix"
+dicom_dir="${a:$(strindex $a "FED"):$(strindex $a "_3T")-$(strindex $a "FED")}$dicom_suffix"
+ln -s "$data_dir$subject_id$dicom_dir" $data_dir$subject_id$suffix$
