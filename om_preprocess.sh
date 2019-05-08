bef3440f441f332e1bb6d4cfd777761c85b4711a
ehoseini <ehoseini@mit.edu>
2019-05-07 20:21:05 -0400

    bugfix

--- om_preprocess.sh
+++ om_preprocess.sh
@@ -22,12 +22,3 @@ dicom_suffix=_dicoms
 echo $SLURM_ARRAY_TASK_ID
 subject_id=$(awk -v var="$SLURM_ARRAY_TASK_ID" 'BEGIN{ RS = "" ; FS = "\n" }{print $var}' subjects.txt)
 echo $subject_id
-#mkdir "$data_dir$subject_id$suffix"
-#analysis_dir="$data_dir$subject_id$suffix"
-#dicom_dir="${a:$(strindex $a "FED"):$(strindex $a "_3T")-$(strindex $a "FED")}$dicom_suffix"
-#ln -s "$data_dir$subject_id$dicom_dir" $data_dir$subject_id$suffix$
-#cd /mindhive/evlab/u/Shared/ANALYSIS
-#module add mit/matlab/2015b
-#matlab -nodisplay -signelCompThread -r "preprocess_PL2017($analysis_dir); \
-#fprintf({'finished',$subject_id, ''\n'});\
-#quit;"
